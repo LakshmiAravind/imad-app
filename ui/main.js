9096cacf917ebc7123661db0b11a6139cd3730ad
LakshmiAravind <30790237+LakshmiAravind@users.noreply.github.com>
2017-08-11 16:28:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-
+/*
 //Change the main text in index html
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
@@ -18,4 +18,20 @@ function moveRight(){
 img.onclick = function (){
     //img.style.marginLeft= "200px";
     var interval = setInterval(moveRight,50);
+};*/
+
+var counter = 0;
+
+var button = document.getElementById("counter");
+button.onclick = function(){
+  
+  // need to req the counter in server js for the current count
+  
+  //capture response in a variable
+  
+  //render on the html at current span
+  counter = counter + 1;
+  var span = document.getElementById("count");
+    span.innerHTML = counter.toString();    
+        
 };
