310bc8d17b33c2124a846cb152272e27f5b441ee
LakshmiAravind <30790237+LakshmiAravind@users.noreply.github.com>
2017-08-11 16:50:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,18 +20,34 @@ img.onclick = function (){
     var interval = setInterval(moveRight,50);
 };*/
 
-var counter = 0;
+//var counter = 0;
 
 var button = document.getElementById("counter");
 button.onclick = function(){
   
-  // need to req the counter in server js for the current count
-  
+  //create request
+  var request = new XMLHttpRequest();
   //capture response in a variable
+  // need to req the counter in server js for the current count 
+  request.onreadystatechange = function (){
+      
+       if (request.readyState === XMLHttpRequest.DONE){
+           if(request.status === 200){
+               var counter = request.responseText;
+               var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+           }
+       }
+      
+  };
+  
+ //make the request
+ request.open('GET','http://lakshmiaravindm.imad.hasura-app.io/counter',true);
+ request.send(null);
   
   //render on the html at current span
-  counter = counter + 1;
-  var span = document.getElementById("count");
-    span.innerHTML = counter.toString();    
+  //counter = counter + 1;
+  //var span = document.getElementById("count");
+    //span.innerHTML = counter.toString();    
         
 };
