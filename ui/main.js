75e15117ff594c224cd95f5751847f4ecda939de
LakshmiAravind <30790237+LakshmiAravind@users.noreply.github.com>
2017-08-11 11:30:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
+
+//Change the main text in index html
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
+
+//Move image
+
+var imgELement= document.getElementById("madi");
+imgElement.onClick = function (){
+    imgElement.style.marginleft= "200px";
+};
