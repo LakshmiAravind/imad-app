55161c62e2b275a82bb5dfde2da38eb1c3e7373f
LakshmiAravind <30790237+LakshmiAravind@users.noreply.github.com>
2017-08-18 12:32:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,9 +133,10 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name',function (req,res){
-   
-   var name = req.params.name;
+//app.get('/submit-name/:name',function (req,res){
+  app.get('/submit-name/',function (req,res){ //url:  submit-name?name = xx query parameter
+   //var name = req.params.name;
+   var name = req.query.name;
    names.push(name);
    res.send(JSON.stringify(names));
 });
